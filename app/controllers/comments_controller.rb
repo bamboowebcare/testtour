6105a97137fefb78d5b35c41b1136e2fb7d0ff03
bamboowebcare <venema@me.com>
2014-05-14 09:25:02 +0200

    Revert a8f695a..7d44721
    This rolls back to commit a8f695ae3050f520d23be460a64ef48848efb86b.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,13 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show]
-
- def create
+  def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:body, :post_id)
-    end
+  def comment_params
+    params.require(:comment).permit(:body, :commenter)
+  end
 end
