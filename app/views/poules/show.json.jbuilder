1e4f62b81909f8b2e56a5d16bdd2867f4c369058
bamboowebcare <venema@me.com>
2014-05-28 15:03:39 +0200

    0.3.2
    Poules and Teams added

---
+++ app/views/poules/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @poule, :id, :name, :encrypted_password, :reset_password_token, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :teams
+
+  resources :poules
+
   resources :comments
 
  devise_for :users
